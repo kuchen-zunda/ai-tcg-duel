468a7ecd14b314b1694419ffe2e6b9d8760ea454
kuchen-zunda <kuchen.de.suman@gmail.com>
2025-08-22 01:00:08 +0900

    update

--- functions/lib/config.js
+++
@@ -1,1 +0,0 @@
-export const REGION = "asia-northeast1";
--- functions/lib/engine/rng.js
+++
@@ -1,5 +0,0 @@
-import seedrandom from "seedrandom";
-export function nextInt(seed, rollNo, max) {
-    const rng = seedrandom(seed + ":" + rollNo);
-    return Math.floor(rng() * max) + 1;
-}
